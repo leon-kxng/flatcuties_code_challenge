f31dc24b35034e26343bd26d626cdd7e7addcae9
Leon <iamleonkigen@gmail.com>
2023-10-25 10:08:21 +0300

    assignment first upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const characterData = () => {
+    // Cache DOM elements
+    const characters = document.getElementById('data');
+    const characterImage = document.getElementById('image');
+    const characterName = document.getElementById('name');
+    const characterVotes = document.getElementById('vote-count');
+    const votesForm = document.getElementById('votes-form');
+    const votes = document.getElementById('votes');
+
+    characters.innerHTML = '';
+
+    let baseURL = '';
+    
+    fetch(baseURL)
+    .then(response => response.json())
+    .then(data => renderCharacterList(data));
+
+    function renderCharacterList(data) {
+        data.forEach(candidate => {
+            const characterList = document.createElement('li');
+            characterList.textContent = candidate.name;
+            characterList.addEventListener('click', () => displayCharacterDetails(candidate));
+            characters.appendChild(characterList);
+        });
+    }
+
+    function displayCharacterDetails(candidate) {
+        characterImage.src = candidate.image;
+        characterName.innerText = candidate.name;
+        characterVotes.innerText = candidate.votes;
+
+        let currentVote = parseInt(characterVotes.textContent, 10);
+
+        votesForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            let newVote = parseInt(votes.value, 10);
+            currentVote = currentVote + newVote;
+            characterVotes.textContent = currentVote;
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', characterData);
